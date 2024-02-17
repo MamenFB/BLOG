d8fb9e3b2b78c5d518581b978018226e8653cfa7
Mamen <fbmaricarmen@gamil.com>
2024-02-17 11:40:45 +0100

    crear pages y rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import SignIn from './pages/SignIn';
-// import Dashboard from './pages/Dashboard';
-// import SignUp from './pages/SignUp';
-// import Projects from './pages/Projects';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import SignUp from './pages/Signup';
+import Projects from './pages/Projects';
 // import Header from './components/Header';
 
 export default function App() {
   return (
-    <h1>app</h1>
-  //   <BrowserRouter >
-  //     <Header />
-  //     <Routes>
-  //       <Route path="/" element={<Home />} />
-  //       <Route path="/about" element={<About />} />
-  //       <Route path="/sign-in" element={<SignIn />} />
-  //       <Route path="/sign-up" element={<SignUp />} />
-  //       <Route path="/dashboard" element={<Dashboard />} />
-  //       <Route path="/projects" element={<Projects />} />
+    
+    <BrowserRouter >
+  {/* //     <Header /> */}
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/about" element={<About />} />
+         <Route path="/sign-in" element={<SignIn />} />
+         <Route path="/sign-up" element={<SignUp />} />
+         <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/projects" element={<Projects />} />
 
-  //     </Routes>
+      </Routes>
 
-  //   </BrowserRouter>
+    </BrowserRouter>
   )
 }

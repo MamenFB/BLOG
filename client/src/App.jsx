770b3b63a321b5e7f5405e1d7d218327bbb292f8
Mamen <fbmaricarmen@gamil.com>
2024-02-18 19:32:25 +0100

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard';
 import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -16,11 +17,12 @@ export default function App() {
          <Route path="/" element={<Home />} />
          <Route path="/about" element={<About />} />
          <Route path="/sign-in" element={<SignIn />} />
-         <Route path="/sign-up" element={<SignUp />} />
+         <Route path='/sign-up' element={<SignUp />} />
          <Route path="/dashboard" element={<Dashboard />} />
          <Route path="/projects" element={<Projects />} />
 
       </Routes>
+      <Footer />
 
     </BrowserRouter>
   )

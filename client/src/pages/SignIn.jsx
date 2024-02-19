83852f81c44ed9960e9558eaeb353e07c8a22961
Mamen <fbmaricarmen@gamil.com>
2024-02-19 22:10:22 +0100

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,15 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 import logo from '../assets/logo1.png';
 
 // Definición del componente SignIn
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   // Hook de React Router para navegar programáticamente
   const navigate = useNavigate();
   // Manejador de cambio en los inputs del formulario
@@ -19,11 +21,10 @@ export default function SignIn() {
     e.preventDefault();
     // Verifica que todos los campos estén llenos
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -31,15 +32,15 @@ export default function SignIn() {
       });
       const data =await res.json();
       if (data.sucess === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+      
       if(res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/'); // Navega al inicio en caso de éxito
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };  
   return (
@@ -47,17 +48,17 @@ export default function SignIn() {
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
         <div className='flex-1'>
-        <Link to="/" className='font-bold dark:text-white text-4xl'>
-        <img src={logo} alt="Logo de Mamen" className='w-8 h-8' style={{ display: 'inline', verticalAlign: 'middle' }} />
-          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-600 to-pink-600 rounded-lg text-white'>
-           Mamen Blog
-          </span>
-        </Link>
-        <p className='text-sm mt-5'>
-            You can sign in with your email and password
-            or with Google.
-        </p>
-        </div>
+          <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <img src={logo} alt="Logo de Mamen" className='w-8 h-8' style={{ display: 'inline', verticalAlign: 'middle' }} />
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-600 to-pink-600 rounded-lg text-white'>
+              Mamen Blog
+            </span>
+          </Link>
+          <p className='text-sm mt-5'>
+              You can sign in with your email and password
+              or with Google.
+          </p>
+          </div>
         {/* right */}
         
         <div className='flex-1'>

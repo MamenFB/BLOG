15a82736d256a9f4dc73cb62258fcd3922aacc24
Mamen <fbmaricarmen@gamil.com>
2024-04-18 14:22:13 +0200

    dashboard cambios

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -49,7 +49,7 @@ export default function DashSidebar() {
               <Sidebar.Item
                 active={tab === 'dash' || !tab}
                 icon={HiChartPie}
-                as='div'
+                as='div' 
               >
                 Panel de control
               </Sidebar.Item>
